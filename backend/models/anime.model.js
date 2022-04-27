437cb6424696d181f0e60d4c534939f0b2eb9050
JeanSolo <jeanpksolo@gmail.com>
2022-04-27 07:30:14 -0400

    fix: rename anime model table name variable to match data

--- backend/models/anime.model.js
+++ backend/models/anime.model.js
@@ -1,8 +1,8 @@
 const knex = require('../knex');
-const CUSTOMER_TABLE = 'anime';
+const ANIME_TABLE = 'anime';
 
 module.exports = {
      getAll(limit = 100) {
-        return knex.select().from(CUSTOMER_TABLE).limit(limit);
-    }
+        return knex.select().from(ANIME_TABLE).limit(limit);
+    },
 }
