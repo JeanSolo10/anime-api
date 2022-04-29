84c81afd8179d31cb6d58cb85298c7f11c422258
JeanSolo <jeanpksolo@gmail.com>
2022-04-29 10:02:43 -0400

    feat: add styling for main page

--- frontend/src/components/AllAnimes.js
+++ frontend/src/components/AllAnimes.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import AnimeForm from "./AnimeForm";
 import AddAnimeButton from "./AddAnimeButton";
+import "../styles/AllAnimes.css";
 
 export default function AllAnimes(props) {
     const [formView, setFormView] = useState();
@@ -10,33 +11,37 @@ export default function AllAnimes(props) {
         <section className="all-anime">
             { formView !== "Form" ?
                 (
-                <div className="all-anime-container">
-                    <AddAnimeButton 
-                        setFormView={setFormView}
-                    />
-                    {animes.map((anime) => {
-                        let {id, name, image_url} = anime;
-                        image_url = image_url ? image_url : '/images/anime_placeholder.jpg';
-                        return (
-                            <div key={`anime${id}`} className="anime">
-                                <h2 className="anime-title">{name}</h2>
-                                <img
-                                    className="anime-image"
-                                    alt="anime view"
-                                    src={image_url}
-                                    onClick={() => {
-                                        setSelectedAnime({
-                                            id: id,
-                                            image: image_url,
-                                            name: name
-                                        });
-                                        setCurrentView("SingleAnime");
-                                    }}
-                                />
-                            </div>
-                        );
-                    })}
-                </div>
+                <>
+                    <AddAnimeButton setFormView={setFormView} />
+                    <div className="all-anime-container">
+                        {animes.map((anime) => {
+                            let {id, name, image_url} = anime;
+                            image_url = image_url ? image_url : '/images/anime_placeholder.jpg';
+                            return (
+                                <div key={`anime${id}`} className="anime">
+                                    <div className="anime-title">
+                                        <h2>{name}</h2>
+                                    </div>
+                                    <div className="anime-images">
+                                        <img
+                                            className="anime-image"
+                                            alt="anime view"
+                                            src={image_url}
+                                            onClick={() => {
+                                                setSelectedAnime({
+                                                    id: id,
+                                                    image: image_url,
+                                                    name: name
+                                                });
+                                                setCurrentView("SingleAnime");
+                                            }}
+                                        />
+                                    </div>
+                                </div>
+                            );
+                        })}
+                    </div>
+                </>
                 ) : (
                     <>
                         <AnimeForm 
