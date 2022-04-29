a388401a66415d8e26b2d47a6c8fbf846442bf25
JeanSolo <jeanpksolo@gmail.com>
2022-04-29 11:48:05 -0400

    feat: add styling for NavBar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,14 +1,15 @@
 import React from "react";
+import '../styles/NavBar.css';
 
 export default function Navbar(props) {
   return (
-    <div className="navbar">
-      <nav className="navbar-header">
-          <div className="github-link">
-            <a href="https://github.com/JeanSolo10/anime-api">Github Repository</a>
-          </div>
+    <div className="navbar-container">
+      <nav className="navbar">
           <div className="api-title">
             <p>Anime-API</p>
+          </div>
+          <div className="github-link">
+            <a href="https://github.com/JeanSolo10/anime-api">GitHub Repository</a>
           </div>
       </nav>
     </div>
