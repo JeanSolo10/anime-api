33ae8a6f0c8d823f283a1a9d59cf6938197ce5df
JeanSolo <jeanpksolo@gmail.com>
2022-04-29 23:18:48 -0400

    feat: add new seed entry

--- db/seeds/001-anime.js.js
+++ db/seeds/001-anime.js.js
@@ -7,6 +7,7 @@ exports.seed = async function(knex) {
   await knex('anime').del()
   await knex('anime').insert([
     {id: 1, name: 'Naruto', image_url: 'https://cdn.myanimelist.net/images/anime/13/17405.jpg'},
-    {id: 2, name: 'Kimetsu no Yaiba', image_url: 'https://cdn.myanimelist.net/images/anime/1286/99889.jpg'}
+    {id: 2, name: 'Kimetsu no Yaiba', image_url: 'https://cdn.myanimelist.net/images/anime/1286/99889.jpg'},
+    {id: 3, name: 'Shingeki no Kyojin', image_url: 'https://cdn.myanimelist.net/images/anime/5/44560.jpg'}
   ]);
 };
