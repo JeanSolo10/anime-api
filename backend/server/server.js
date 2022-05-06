fadf6b935aa09f04e13f1d367e4d68090dbb990a
JeanSolo <jeanpksolo@gmail.com>
2022-05-06 16:46:45 -0400

    refactor: update response for build

--- backend/server/server.js
+++ backend/server/server.js
@@ -19,7 +19,7 @@ app.use('/api/v1/reviews/', reviewController);
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.resolve(__dirname, "../..", "frontend", "build")));
     app.get('*', (req, res) => {
-        req.sendFile(path.resolve(__dirname, "../..", "frontend", "build", "index.html"))
+        res.sendFile(path.resolve(__dirname, "../..", "frontend", "build", "index.html"))
     });
 }
 
